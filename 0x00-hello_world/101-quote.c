a521481fadbdc66cfb5d7ba22dbbe707970ab2e8
Queendlin Ozegbe <queendalynoz@gmail.com>
2023-02-23 03:09:26 -0800

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,7 +6,8 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19
-	return (0);
+	write(2,
+	"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", $9);		
+	return (1);
 }
 
