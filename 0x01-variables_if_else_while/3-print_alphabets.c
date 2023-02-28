a37c7d3bcdf1518becc5080712bd05914e0326e8
Queendlin Ozegbe <queendalynoz@gmail.com>
2023-02-28 14:40:46 -0800

    Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
+	//M-x delete-trailing-whitespace//
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
        int i;
 
        for (i = 0; i < 52; i++)
-       {       
+       {      
 	       putchar(alp[i]);
        }
        putchar('\n');
