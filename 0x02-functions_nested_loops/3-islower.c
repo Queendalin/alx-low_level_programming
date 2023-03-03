e8a3b6cdfce7826e12f895aa90f410736ab7f6d1
Queendlin Ozegbe <queendalynoz@gmail.com>
2023-03-02 22:26:06 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _is lower - check if a char is lowercase
- *@c: is the char to be checked
+ * _islower - check if a char is lowercase
+ *@c: is the char to be checked.
  * Return: 1 if char is lowercase, otherwise 0.
  */
 
