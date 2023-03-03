cb1e2ebade73f874ad95b4e8c4ab0620ea068b0c
Queendlin Ozegbe <queendalynoz@gmail.com>
2023-03-03 13:18:02 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - prints the last digit of a randomly generated number
  *and whether it is greater than 5, less than 6, or 0.
- * Return: Always 0 (Success)
+ * Return: Always 0.
  *
  */
 int main(void)
@@ -14,9 +14,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if ((n % 10) > 5)
-	{
-	       printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-       int i;
+        int i:
+		
+		for (i = 0; i < 52; i++)
+	{
+		putcha(alp[i]);
+	}
 
-       for (i = 0; i < 52; i++)
-       {      
-	       putchar(alp[i]);
-       }
-       putchar('\n');
-       return (0);
-}
