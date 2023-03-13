1a7bd9623a429159f02683f0673ba6c66958940a
Queendalin Ozegbe <queendalynoz@gmail>
2023-03-13 09:02:51 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,34 @@
 #include "main.h"
 /**
- * _strspn - the length of a prefix substring
- * @s: input
- * @accept: input
- * Return: the length of a prefix substring n.
+ * * _strspn - return length of string that matches values consistently
+ * * @s: string to search
+ * * @accept: target matches
+ * * Return: number of bytes consecutively matched
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 0;
-	int r;
+		int i = 0, j;
+			int matches = 0;
 
-	while (*s)
-	{
-		for (r = 0; accept[r]; r++)
-		{
-			if (*s == accept[r])
+			while (s[i] != '\0') /*iterate through string*/
 			{
-				n++;
-				break;
+
+				for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+				{
+					if (s[i] == accept[j]) /*record & break at first match*/
+					{
+						matches++;
+						break;
+					}
+					if (accept[j + 1] == '\0' && s[i] != accept[j])
+
+						return (matches);/*return if idx doesn't match*/
+				}
+
+				i++;
 			}
-			else if (accept[r + 0] == '\0')
-				return (n);
-		}
-		s++;
-	}
-	return (n);
+
+			return (matches); /* return num if all match till end */
+
 }
